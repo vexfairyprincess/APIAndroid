83de54602ee8a0f915b772f8f242f24d4064bb9a
IGChBl <ichavarria@uamv.edu.ni>
2025-05-22 18:26:49 -0600

    ProductRepository

---
+++ app/src/main/java/com/example/apiandroid/services/ProductRepository.kt
@@ -0,0 +1,9 @@
+package com.example.apiandroid.services
+
+import com.example.apiandroid.dataclass.Product
+
+
+class ProductRepository(private val api: ApiService) {
+    suspend fun fetchAll(): List<Product> = api.getAll()
+    suspend fun add(product: Product): Product = api.create(product)
+}
